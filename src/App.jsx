081fa870fca432c82e6b3ecca61b1e4e26184421
Ashley K Motsie <motsieashley31@gmail.com>
2025-08-16 19:49:44 +0200

    Initial commit: Smith ChatBot

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import ChatBotIcon from "./components/ChatBotIcon"
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="chatbot-popup">
+        <div className="chat-header">
+          {/* ChatBot header */}
+          <div className="header-info">
+            <ChatBotIcon />
+            <h2 className="logo-text">Smith-ChatBot</h2>
+          </div>
+          <button className="material-symbols-rounded">Keyboard_arrow_down</button>
+        </div>
+
+        {/* ChatBot body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatBotIcon />
+            <p className="message-text">
+              Hey there!. <br /> How can I help you today?
+            </p>
+          </div>
+
+          <div className="message user-message">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, 
+              error corrupti laboriosam officiis aperiam asperiores temporibus aspernatur veniam odit mollitia, 
+              recusandae labore molestias minus inventore optio neque consequatur, ad quibusdam?
+            </p>
+          </div>
+        </div>
+
+        {/* ChatBot footer */}
+        <div className="chat-footer">
+          <form action="" className="chat-for">
+            <input type="text" className="message-input" placeholder="Message...." required/>
+            <button className="material-symbols-rounded">arrow_upward</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
