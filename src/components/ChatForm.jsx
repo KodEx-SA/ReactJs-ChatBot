dd35eb98a6596c3bf0f4c3bf67321540fb3961e9
Ashley K Motsie <motsieashley31@gmail.com>
2025-08-16 23:36:26 +0200

    Initital Updates commit

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,7 +1,21 @@
+import React, { useRef } from "react";
+
 const ChatForm = () => {
+    const inputRef = useRef();
+    
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        const userMessage = inputRef.current.value.trim();
+        if (!userMessage) return;
+        inputRef.current.value = "";
+        
+        console.log(userMessage);
+    }
+
     return (
-        <form action="" className="chat-form">
-            <input type="text" className="message-input" placeholder="Message...." required />
+        <form action="" className="chat-form" onSubmit={handleFormSubmit}>
+            <input ref={inputRef} type="text" className="message-input" placeholder="Message...." required />
             <button className="material-symbols-rounded">arrow_upward</button>
         </form>
     )
